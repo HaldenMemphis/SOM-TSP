1ba897851aff17b393aaba4fd22ccf199aeefaec
flash666404 <115422118+flash666404@users.noreply.github.com>
2023-05-06 23:00:57 +0100

    Update plot_network.py

--- src/plot_network.py
+++ src/plot_network.py
@@ -1,34 +1,13 @@
 import matplotlib.pyplot as plt
-import matplotlib as mpl
 
-def scatter_cities(cities, ax):
-    ax.scatter(cities['x'], cities['y'], color='red', s=4)
+def plot_city_neurons(city_df, neuron_coords, figname='network.png'):
 
-def plot_neurons(neurons, ax):
-    ax.plot(neurons[:,0], neurons[:,1], 'r.', ls='-', color='#0063ba', markersize=2)
+    plt.scatter(city_df['x'], city_df['y'], c='b', label='City')
+    plt.scatter(neuron_coords[:, 0], neuron_coords[:, 1], c='r', label='Neuron')
 
-def save_figure(fig, name):
-    plt.savefig(name, bbox_inches='tight', pad_inches=0, dpi=200)
+    # 添加图例和标题
+    plt.legend()
+    plt.title('City and Neuron Coordinates')
+    plt.savefig(figname, bbox_inches='tight', pad_inches=0)
     plt.close()
-
-def plot_network(cities, neurons, name='diagram.png', ax=None):
-    """Plot a graphical representation of the problem"""
-    mpl.rcParams['agg.path.chunksize'] = 10000
-
-    if not ax:
-        fig = plt.figure(figsize=(5, 5), frameon = False)
-        axis = fig.add_axes([0,0,1,1])
-
-        axis.set_aspect('equal', adjustable='datalim')
-        plt.axis('off')
-
-        scatter_cities(cities, axis)
-        plot_neurons(neurons, axis)
-
-        save_figure(fig, name)
-
-    else:
-        scatter_cities(cities, ax)
-        plot_neurons(neurons, ax)
-        return ax
 
