94f49cd245dd789b2119dc963dc402eb3ae67cb6
flash666404 <115422118+flash666404@users.noreply.github.com>
2023-05-06 23:01:12 +0100

    Update plot_route.py

--- src/plot_route.py
+++ src/plot_route.py
@@ -1,35 +1,16 @@
 import matplotlib.pyplot as plt
-import matplotlib as mpl
 
-def scatter_cities(cities, ax):
-    ax.scatter(cities['x'], cities['y'], color='red', s=4)
+def plot_route(city_df, route, figname='route.png'):
+    plt.scatter(city_df['x'], city_df['y'], c='b', label='City')
 
-def plot_route_lines(route, ax):
-    route.loc[route.shape[0]] = route.iloc[0]
-    ax.plot(route['x'], route['y'], color='purple', linewidth=1)
+    for i in range(len(route) - 1):
+        start = route[i]
+        end = route[i + 1]
+        plt.plot([city_df.loc[start, 'x'], city_df.loc[end, 'x']],
+                 [city_df.loc[start, 'y'], city_df.loc[end, 'y']],
+                 c='r', linewidth=2)
 
-def save_figure(fig, name):
-    plt.savefig(name, bbox_inches='tight', pad_inches=0, dpi=200)
-    plt.close()
-
-def plot_route(cities, route, name='diagram.png', ax=None):
-    """Plot a graphical representation of the route obtained"""
-    mpl.rcParams['agg.path.chunksize'] = 10000
-
-    if not ax:
-        fig = plt.figure(figsize=(5, 5), frameon = False)
-        axis = fig.add_axes([0,0,1,1])
-
-        axis.set_aspect('equal', adjustable='datalim')
-        plt.axis('off')
-
-        scatter_cities(cities, axis)
-        plot_route_lines(cities.reindex(route), axis)
-
-        save_figure(fig, name)
-
-    else:
-        scatter_cities(cities, ax)
-        plot_route_lines(cities.reindex(route), ax)
-        return ax
+    plt.legend()
+    plt.title('City and Route Coordinates')
+    plt.savefig(figname, bbox_inches='tight', pad_inches=0)
 
